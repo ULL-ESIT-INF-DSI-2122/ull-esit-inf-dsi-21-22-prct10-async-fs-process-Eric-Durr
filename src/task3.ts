4538ff714e936705c55e02b9df56fa7e2f1247d0
Eric DS <eric.d.s.info@gmail.com>
2022-04-30 10:38:36 +0100

    complete task 3

--- src/task3.ts
+++ src/task3.ts
@@ -2,6 +2,6 @@ import WatchUserClass from './watchUser.class';
 
 const usersWatcher: WatchUserClass = new WatchUserClass(process.argv[2]);
 
-if (!usersWatcher.watcher) {
-  console.log(`error al abrir el fichero ${process.argv[2]}`);
+if (!usersWatcher.singleUserWatcher) {
+  console.error(`ERROR: user ${process.argv[2]} was not found`);
 }
--- src/watchUser.class.ts
+++ src/watchUser.class.ts
@@ -1,4 +1,5 @@
 import fs, { existsSync, watch } from 'fs';
+import { Note } from './notes/Note.class';
 /**
  * # Watch User Class | Primary parent class
  * This class watches changes on a certain user notes and notify them
@@ -9,40 +10,79 @@ import fs, { existsSync, watch } from 'fs';
  *
  * ## Methods
  * - user(void) | returns String with user name
- * - watcher(void) | returns boolean of execution, runs directory watcher events
+ * - singleUserWatcher(void) | returns boolean of execution, runs directory singleUserWatcher events
  */
 
 export default class WatchUserClass {
-  private _user: string;
+  private readonly _user: string;
+
+  private _filesList: string[];
 
   constructor(user: string) {
     this._user = user;
+    this._filesList = fs.readdirSync(`./database/${this.user}`);
   }
 
   get user(): string { return this._user; }
 
-  get watcher(): boolean {
+  get filesList(): string[] { return this._filesList; }
+
+  private updateList(): void { this._filesList = fs.readdirSync(`./database/${this.user}`); }
+
+  get singleUserWatcher(): boolean {
     if (existsSync(`./database/${this.user}`)) {
       console.log(`Watching notes changes for ${this.user}`);
-      let prevFileNames: string[] = fs.readdirSync(`./database/${this.user}`);
       watch(`./database/${this.user}`, (eventType, filename) => {
-        console.log(`\nChange on ${this.user} notes was detected`);
         if (eventType === 'rename') {
-          const currFileNames: string[] = fs.readdirSync(`./database/${this.user}`);
-          if (currFileNames.length > prevFileNames.length) {
-            console.log(`A new note was created for ${this.user}, new note is ${filename}`);
-          }
-          if (currFileNames.length < prevFileNames.length) {
-            console.log(`Note ${filename} was deleted for ${this.user}`);
-          }
-          prevFileNames = fs.readdirSync(`./database/${this.user}`);
+          console.log(`\nRename on ${this.user} notes was detected`);
+          this.manageRename(filename);
         }
         if (eventType === 'change') {
+          console.log(`\nChange on ${this.user} notes was detected`);
           console.log(`Note ${filename} was edited for ${this.user}`);
         }
       });
       return true;
     }
     return false;
+  }
+
+  get fullDirectoryWatcher(): boolean {
+    if (existsSync(`./database/${this.user}`)) {
+      console.log('Watching notes changes for complete directory');
+      watch(
+        './database',
+        // launches error ERR_FEATURE_UNAVAILABLE_ON_PLATFORM('watch recursively')
+        // { recursive: true },
+        (eventType, filename) => {
+          if (eventType === 'rename') {
+            console.log('\nRename on notes directory was detected');
+            this.manageRename(filename.toString());
+          }
+          if (eventType === 'change') {
+            console.log('\nChange on notes directory was detected');
+            console.log(`Note ${filename} was edited for ${this.user}`);
+          }
+        },
+      );
+      return true;
+    }
+    return false;
+  }
+
+  private manageRename(filename: string) {
+    const currFileNames: string[] = fs.readdirSync(`./database/${this.user}`);
+
+    if (currFileNames.length > this.filesList.length) {
+      console.log(`A new note was created for ${this.user}, new note is ${filename}`);
+      // console.log('The file contains:');
+      // console.log(fs
+      //   .readFileSync(`./database/${this.user}/${filename}`)
+      //   .toString());
+    }
+    if (currFileNames.length < this.filesList.length) {
+      console.log(`Note ${filename} was deleted for ${this.user}`);
+    }
+    this.updateList();
   }
 }
